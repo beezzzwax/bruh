c3e34df4a41b54cb097d9bc85125f593c355cb04
beezzzwax <612brandonly@gmail.com>
2026-02-11 20:23:59 +1100

    cccsccs

--- script.js
+++ script.js
@@ -5,8 +5,3 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
-const apiKey= "960575f5688b5edd68105b8c9114e151"
-const apiUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-const searchBox= document.querySelector(".search input");
-const searchBtn= document.querySelector(".search button");
-const weatherIcon= document.querySelector(".weather-icon");
